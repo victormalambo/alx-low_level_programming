c4df70298891cb8eaf2403907ea7ef5d45e31cc8
victormalambo <victor.malambo23@gmail.com>
2023-04-04 05:10:39 -0700

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strpbrk(char *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *haystack, char *needle);
 void Print_chessBoard(char (*a)[8]);
