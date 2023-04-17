34633720822e8eedb2d1a0a8bed5b8d8a0d4f444
victormalambo <victor.malambo23@gmail.com>
2023-04-17 06:42:17 -0700

    task squarerroot

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,7 +7,7 @@ int actual_sqrt_recursion(int n, int i);
  * @n: number to be calculated
  * Return: the result
  */
- int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
