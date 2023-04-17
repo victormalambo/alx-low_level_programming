9d93df1566ff8c877c7232f853832e6b631827d3
victormalambo <victor.malambo23@gmail.com>
2023-04-17 03:06:18 -0700

    Task pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _pow_recursion - return value of a raised to b
- * @a: value to raise another value
- * @b: power to be raised
+ * _pow_recursion - return value of x raised to y
+ * @x: value to raise another value
+ * @y: power to be raised
  * Return: result of the power
  */
-int _pow_recursion(int a, int b)
+int _pow_recursion(int x, int y)
 {
-	if (b < 0)
+	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return (a * _pow_recursion(a, b - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
