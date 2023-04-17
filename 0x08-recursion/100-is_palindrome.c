54f8b2bdb92b1c36c5855f0841f1b4515f91fc3d
victormalambo <victor.malambo23@gmail.com>
2023-04-17 07:24:21 -0700

    task is tough

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -23,7 +23,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1+ _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks characters for palindrome
@@ -34,7 +34,7 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-	if (*(s + 1) != *(s + len - 1))
+	if (*(s + i) != *(s + len - 1))
 		return (0);
 	if (i >= len)
 		return (1);
