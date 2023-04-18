b6585195ceec563ff6fd110bb6da52e2dcdbdbd0
victormalambo <victor.malambo23@gmail.com>
2023-04-18 00:28:12 -0700

    task dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,6 +11,7 @@ int _strlen(char *s)
 {
 
 	int i;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -30,6 +31,7 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 
 	while (src[len] != '\0')
@@ -55,6 +57,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 
 	dog_t *dog;
+
 	int len1, len2;
 
 	len1 = _strlen(name);
